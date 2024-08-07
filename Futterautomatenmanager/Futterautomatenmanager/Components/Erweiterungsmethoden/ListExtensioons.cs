36c836868167f0413a60714d5461665de10754c6
BauerPhilipp <fam.bauer@live.at>
2024-08-07 10:47:20 +0200

    Erweiterungsmethode bei List<T> integriert

---
+++ Futterautomatenmanager/Futterautomatenmanager/Components/Erweiterungsmethoden/ListExtensioons.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Futterautomatenmanager.Components.Erweiterungsmethoden
+{
+    public static class ListExtensions
+    {
+        public static List<T> LastElementFirst<T>(this List<T> list)
+        {
+            var lastElement = list.Last();
+            list.RemoveAt(list.Count -1);
+            list.Insert(0, lastElement);
+            return list;
+        }
+    }
+}
